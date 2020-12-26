ba8cda32117c492d91a953c8c59e36ee68a5c96e
Seth Goerke <sethgoerke@hotmail.com>
2020-12-26 13:45:23 -0600

    through 5

--- Desktop/orbit-report/src/app/orbit-list/orbit-list.component.ts
+++ Desktop/orbit-report/src/app/orbit-list/orbit-list.component.ts
@@ -7,9 +7,10 @@ import { Satellite } from '../satellite';
   templateUrl: './orbit-list.component.html',
   styleUrls: ['./orbit-list.component.css']
 })
+
 export class OrbitListComponent implements OnInit {
-  
   @Input() satellites: Satellite[];
+  
   constructor() {}
 
   ngOnInit() {
